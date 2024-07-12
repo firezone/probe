28adb3be450be198c74f329c6b3d79f89cd01de0
Andrew <a@firezone.dev>
2024-07-11 18:41:15 -0600

    Use Fly request ID for logs

--- lib/probe/endpoint.ex
+++ lib/probe/endpoint.ex
@@ -36,7 +36,7 @@ defmodule Probe.Endpoint do
     param_key: "request_logger",
     cookie_key: "request_logger"
 
-  plug Plug.RequestId
+  plug Plug.RequestId, http_header: "fly-request-id"
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
